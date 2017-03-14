10a0905e580b67858bdbc3b581161bd8f04d1eee
Chloe <chenderson126+bitbucket@gmail.com>
2017-03-14 16:42:05 -0500

    Refactored app.js with atom-beautify

--- public/app.js
+++ public/app.js
@@ -3,25 +3,25 @@ xhr.open('GET', '/projects/');
 xhr.send(null);
 
 xhr.onreadystatechange = function() {
-  var DONE = 4; // readyState 4 means the request is done.
-  var OK = 200; // status 200 is a successful return.
-  if (xhr.readyState === DONE) {
-    if (xhr.status === OK) {
-      console.log(xhr.responseText); // 'This is the returned text.'
-      var projects = JSON.parse(xhr.responseText);
-      projects.forEach(function(project){
-        var name = document.createElement('a');
-        name.innerHTML = project.name;
-        name.href = "/projects/" + project.id;
-        document.body.appendChild(name);
-        project.onClick = function(event) {
-          event.preventDefault();
-          alert("Load using Ajax");
-        }
-      });
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+            console.log(xhr.responseText); // 'This is the returned text.'
+            var projects = JSON.parse(xhr.responseText);
+            projects.forEach(function(project) {
+                var name = document.createElement('a');
+                name.innerHTML = project.name;
+                name.href = "/projects/" + project.id;
+                document.body.appendChild(name);
+                project.onClick = function(event) {
+                    event.preventDefault();
+                    alert("Load using Ajax");
+                }
+            });
 
-    } else {
-      console.log('Error: ' + xhr.status); // An error occurred during the request.
+        } else {
+            console.log('Error: ' + xhr.status); // An error occurred during the request.
+        }
     }
-  }
 }
