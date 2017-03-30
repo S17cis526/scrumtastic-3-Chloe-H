dbb3317988c50fde5f65316e0899e11ed82b26e0
Chloe <chenderson126+bitbucket@gmail.com>
2017-03-30 14:11:24 -0500

    I'm pretty sure I'm still missing something, especially since nothing appears when I start the server and load the page.

---
+++ client/app.js
@@ -0,0 +1,8 @@
+/* Main entry point */
+
+var project = require('./project');
+
+
+$.get('/projects', function(projects) {
+    $('body').html(project.list(projects));
+});
---
+++ client/project.js
@@ -0,0 +1,18 @@
+module.exports = {
+    list
+};
+
+
+/* Given a list of projects as JSON, create a table */
+function list(projects) {
+    var table = $('<table>').class('table');
+    var head = $('<tr>').append('<th>Name</th>').appendTo(table);
+
+    projects.forEach(function(project) {
+        var row = $('<tr>').append(
+            $('<td>').text(project.name)
+        ).appendTo(table);
+    });
+    
+    return table;
+}
